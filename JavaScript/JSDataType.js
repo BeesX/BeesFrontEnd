ca68d18afa05fe606df6011aba9fa6d61829ff9b
guoxiaoxing <guoxiaoxing@souche.com>
2017-02-22 13:56:56 +0800

    add ECMAScript 6 document

--- JavaScript/JSDataType.js
+++ JavaScript/JSDataType.js
@@ -6,20 +6,19 @@ var a = "3.14";
 var b = a - 2;
 var c = a + 2;
 
-// alert(b + "\n" + c);
-
-var scope = void(b = 2, c = 3, d = 4);
-
 var test = function(){
+    var scope = "局部变量变量";
+    document.writeln(typeof 1 + '<br/>');
+    document.writeln(typeof true + "<br/>");
+    document.writeln(typeof test + "<br/>");
+    document.writeln(typeof null + "<br/>");
+    document.writeln(typeof 13 + "<br/>");
+    document.writeln(typeof 1 + "<br/>");
+    document.writeln(0.05 + 0.25);
 
-    document.writeln(scope + "<br/>");
-
-    scope = "局部变量变量";
-
-    document.writeln(scope + "<br/>");
 };
 
-
-if(typeof test())
+document.writeln(typeof message + "<br/>");
 
 test();
+
