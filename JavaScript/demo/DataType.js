95e58f43e87c8b4639c0c00bacd205e30406435c
guoxiaoxing <guoxiaoxingse@gmail.com>
2017-05-10 09:01:36 +0800

    add js document

--- JavaScript/demo/DataType.js
+++ JavaScript/demo/DataType.js
@@ -6,6 +6,7 @@ var a = "3.14";
 var b = a - 2;
 var c = a + 2;
 var colors = ["red", "green", "blue"];
+var numbers = [1, 2, 3, 4, 5, 10];
 
 var writeDataType = function(){
     var scope = "局部变量变量";
@@ -18,7 +19,17 @@ var writeDataType = function(){
     document.writeln(0.05 + 0.25);
     document.writeln(typeof message + "<br/>");
     document.writeln(colors.toString() + "<br/>");
+    document.writeln("push：" + colors.push("black","yellow") + "--" + colors.toString()+ "<br/>");
+    document.writeln("pop：" + colors.pop() + "--" + colors.toString()+ "<br/>");
+    document.writeln("shift：" + colors.shift() + "--" + colors.toString()+ "<br/>");
+    document.writeln("unshift：" + colors.unshift("black","yellow") + "--" + colors.toString()+ "<br/>");
+    document.writeln("reverse：" + numbers.reverse() + "--" + numbers.toString()+ "<br/>");
+    document.writeln("sort：" + numbers.sort(compare) + "--" + numbers.toString()+ "<br/>");
 };
+
+function compare(value1, value2) {
+    return value1 - value2;
+}
 
 writeDataType();
 
