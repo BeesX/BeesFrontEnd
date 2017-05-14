85550c2c59804f4153428d9fd71729227255d90d
guoxiaoxing <guoxiaoxingse@gmail.com>
2017-05-14 13:54:28 +0800

    add js date type document

--- JavaScript/demo/Function.js
+++ JavaScript/demo/Function.js
@@ -8,13 +8,30 @@ function writeFunction() {
         return num1 + num2;
     }
 
+    function callSum1(num1, num2) {
+        return sum.apply(this, arguments);
+    }
+
+    function callSum2(num1, num2) {
+        return sum.apply(this, arguments);
+    }
+
+    function callSum2(num1, num2) {
+        return sum.apply(this, [num1, num2]);
+    }
+
+
     document.writeln("sum：" + sum(1, 2) + "<br/>");
 
     var sum1 = sum;
     document.writeln("sum1：" + sum1(1, 2) + "<br/>");
 
-    sum = null;
     document.writeln("sum1：" + sum1(1, 2) + "<br/>");
+
+    document.writeln("length：" + sum.length + "<br/>");
+    document.writeln("length：" + sum.prototype + "<br/>");
+    document.writeln("callSum1(1, 2) ：" + callSum1(1, 2) + "<br/>");
+    document.writeln("callSum2(1, 2) ：" + callSum2(1, 2) + "<br/>");
 }
 
 writeFunction();
