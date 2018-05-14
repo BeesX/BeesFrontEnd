548ace9869e2f7a27774e1563bf6056c96ff517b
苏策 <xiaoxing.gxx@alibaba-inc.com>
2018-05-14 21:11:08 +0800

    document: es6

---
+++ JavaScript/demo/ES6.js
@@ -0,0 +1,28 @@
+/**
+ * Created by suce on 2018/5/14.
+ */
+
+
+const PI = 3.14;
+
+document.writeln("<br/>");
+document.writeln(PI);
+
+let a = 5;
+
+'abc'.charAt(0);
+
+function point(x = 0, y = 0){
+    this.x = x;
+    this.y = y;
+}
+
+document.writeln("<br/>");
+document.writeln(point.length);
+
+var target = {};
+
+Object.assign(target, {c : 3});
+
+document.writeln("<br/>");
+document.writeln(target.c);
